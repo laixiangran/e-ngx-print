7b73bc5dde908010a7b437d086de7f5c44030ce9
laixiangran <laixiangran@163.com>
2017-04-23 18:59:50 +0800

    ...

--- index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Created by laixiangran on 2017/4/15.
- * homepage：http://www.laixiangran.cn
- */
-
-export {EssenceNg2PrintModule} from './src/essence-ng2-print.module';
-export {EssenceNg2PrintComponent} from './src/essence-ng2-print.component'
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by laixiangran on 2017/4/15.
+ * homepage：http://www.laixiangran.cn
+ */
+
+export {EssenceNg2PrintModule} from './essence-ng2-print.module';
+export {EssenceNg2PrintComponent} from './essence-ng2-print.component'
